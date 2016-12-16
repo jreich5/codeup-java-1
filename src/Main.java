3f4d647529b2e4645cc72a8edf5cfd9d579e25d7
Justin Reich <justinreich.dev@gmail.com>
2016-12-16 10:33:19 -0600

    5.1 exercise

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // INTRO
+        //System.out.println("Hello World!");
+
+
+        // 5.1 - Syntax and Types
+        /*
+        byte byteNumber = 2;
+        System.out.println(byteNumber);
+
+        short shortNumber = 300;
+        System.out.println(shortNumber);
+
+        int intNumber = 50000;
+        System.out.println(intNumber);
+
+        long longNumber = 43912347821347529L;
+        System.out.println(longNumber);
+
+        float floatNumber = (float) 4.39;
+        System.out.println(floatNumber);
+
+        double doubleNumber = 4.1289252858987273894783928738491;
+        System.out.println(doubleNumber);
+
+        char charThing = 'j';
+        System.out.println(charThing);
+
+        intNumber = 'h';
+        System.out.println(intNumber);
+
+        //int class = 34;
+
+        int nullNumber;
+        System.out.println(nullNumber);
+        */
+
+        // 5.2 - Console IO
+
+        
     }
 }
