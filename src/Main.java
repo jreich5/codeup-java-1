9b8b424ddd38169ea66da425cfb92009602c8d1b
Justin Reich <justinreich.dev@gmail.com>
2016-12-30 08:59:06 -0600

    All Pig Latin bonuses except multiple words

--- src/Main.java
+++ src/Main.java
@@ -224,25 +224,26 @@ public class Main {
         */
 
         /*
-        String message = "I can do back slashes \\, and double back slashes \\\\, and the amzing triple back-slash \\\\\\";
+        String message = "I can do back slashes \\, and double back slashes \\\\, and the amazing triple back-slash \\\\\\";
         System.out.println(message);
         */
 
         // Pig Latin Program
 
-        String continueGame = "y";
+        String continueApp = "y";
 
-        while (continueGame.equals("y")) {
+        while (continueApp.equals("y")) {
 
             int lowestVowelIndex = 10;
             String translatedInput = null;
-            char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+            char[] vowels = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
+            char[] digits = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
             // take string input from user
             String userInput;
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter a word to translate to Pig Latin: ");
-            userInput = sc.next().toLowerCase();
+            userInput = sc.next();
 
             // retrieves the index of the first vowel
             for (int i = 0; i < vowels.length; i++) {
@@ -253,21 +254,27 @@ public class Main {
             }
 
             // store certain value of translated word based on lowestVowelIndex position
-            if (lowestVowelIndex > 0) {
+            if (lowestVowelIndex > 0 && lowestVowelIndex < 10) {
                 translatedInput = userInput.substring(lowestVowelIndex, userInput.length()) + userInput.substring(0, lowestVowelIndex) + "ay";
             } else {
                 translatedInput = userInput + "way";
+            }
+
+            for (int i = 0; i < digits.length; i++) {
+                if (userInput.indexOf(digits[i]) != -1) {
+                    translatedInput = userInput;
+                    break;
+                }
             }
 
             // print output
             System.out.println(translatedInput);
 
             System.out.print("Do you want to enter another word (y/n)? ");
-            continueGame = sc.next().toLowerCase();
+            continueApp = sc.next().toLowerCase();
 
         }
         System.out.println("Loop is ended.");
-        System.out.print(continueGame);
 
 
         // for each whitespace occurrence
